46a7fa881a92cf263cf82d4e76747cc5c3e3a22d
MoFololo <116032908+MoFololo@users.noreply.github.com>
2026-07-29 18:02:09 +0800

    Create Swift Project

---
+++ LifeMedals/LifeMedals/LifeMedalsApp.swift
@@ -0,0 +1,17 @@
+//
+//  LifeMedalsApp.swift
+//  LifeMedals
+//
+//  Created by David Da Lian on 2026/7/29.
+//
+
+import SwiftUI
+
+@main
+struct LifeMedalsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
